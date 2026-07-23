32a5579011bd3fd375eee585d5d4a279a7b271d0
Umesh <umesh.sharmaphurailatpam@bitbasegroup.com>
2026-07-23 21:52:57 +0530

    splash

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,17 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 
+import { SplashScreen } from '@capacitor/splash-screen';
+import { provideAppInitializer } from '@angular/core';
+
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular({ mode: 'ios' }),
     provideRouter(routes, withPreloading(PreloadAllModules)),
     provideAnimations(),
+    provideAppInitializer(async () => {
+      await SplashScreen.hide();
+    }),
   ],
 }).catch((err) => console.error(err));
